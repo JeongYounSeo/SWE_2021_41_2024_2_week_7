23092a7a73d3e16b18b792c7b934bcdb4b058bc6
JeongYounSeo <wjddbstj2000@gmail.com>
2024-10-17 08:46:39 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def twoSum(nums: list[int], target : int) -> list[int]:
+    ret_Pair=[]
+
+    twoNums=[[[first_val, fisrt_idx], [second_val, second_idx]]
+    for fisrt_idx, first_val in enumerate(nums)
+    for second_idx, second_val in enumerate(nums) if fisrt_idx<second_idx]
+    '''twoNums( [ pair1, index_of_pair1 ], [ pair2, index_of_pair2 ] )'''
+    for pair in twoNums:
+        Sum_of_pair=pair[0][0]+pair[1][0]
+        '''Sum_of_pair=pair1+pair2'''
+        if(Sum_of_pair==target):
+            '''if Sum_of_pair == target 
+            then append index of 2 pair in return_Pair'''
+            ret_Pair.append(pair[0][1])
+            ret_Pair.append(pair[1][1])
+    return ret_Pair
